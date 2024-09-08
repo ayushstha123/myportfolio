1c7c12ed611fe5e8912d520c695931700ddda34c
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-09-08 07:40:34 +0545

    change number 1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,13 @@ const Home = () => {
   return (
         <div className='flex flex-col w-2/3 h-2/6'>
          
-          <model-viewer src="./files/skull.glb" ar ar-modes="scene-viewer webxr quick-look" camera-controls tone-mapping="neutral" poster="poster.png" shadow-intensity="1">
-          </model-viewer>        
+
+          <model-viewer src="./files/ship.glb" ar ar-modes="scene-viewer webxr quick-look" camera-controls tone-mapping="neutral" poster="poster.png" shadow-intensity="1">
+          </model-viewer> 
+          <model-viewer src="./files/Eye.glb" ar ar-modes="scene-viewer webxr quick-look" camera-controls tone-mapping="neutral" poster="poster.png" shadow-intensity="1">
+          </model-viewer> 
+          <model-viewer src="./files/tank.glb" ar ar-modes="scene-viewer webxr quick-look" camera-controls tone-mapping="neutral" poster="poster.png" shadow-intensity="1">
+          </model-viewer>             
         </div>
      
   );
